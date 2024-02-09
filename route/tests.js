90a1d192c00ade663c6f0da47368649135183ecf
7mishra <mishrasaptanshu@gmail.com>
2024-02-09 22:50:43 +0530

    added more functionality

--- route/tests.js
+++ route/tests.js
@@ -10,14 +10,25 @@ router.get('/questions', async (req,res) => {
 
 router.post('/startTest', async (req,res) => {
     console.log(req.body)
-    id =  await StartUserTest(req.body.user, req.body.questionIds, req.body.timestamp)
+    const id =  await StartUserTest(req.body.user, req.body.questionIds, req.body.timestamp)
     res.send(id).status(200)
 })
 
 router.post('/endTest', async (req,res) => {
-    console.log(req.body)
-    x = await EndUserTest('asasa', req.body.userTestId , req.body.timestamp,req.body.TestData)
-    res.send(x).status(200)
+    let questionData = {}
+    for(let i=0;i<req.body.TestData.length;i++) {
+        questionData[req.body.TestData[i].id] = req.body.TestData[i].selectedOption
+    }
+    console.log(questionData)
+    let score = await EndUserTest('asasa', req.body.userTestId , req.body.timestamp, questionData)
+    console.log(score)
+    res.send({'score':score})
+})
+
+router.get('/performance', async (req,res) => {
+    let score = await EndUserTest('asasa', req.body.userTestId , req.body.timestamp, questionData)
+    console.log(score)
+    res.send({'score':score})
 })
 
 module.exports = router
--- services/testService.js
+++ services/testService.js
@@ -12,6 +12,7 @@ async function GetQuestions() {
         if (questions == null) {
             throw new Error("Something went wrong")
         }
+        console.log(questions)
         let responseDataList = []
         for (let i = 0; i < questions.length; i++) {
             let responseData = {};
@@ -35,18 +36,15 @@ async function GetQuestions() {
 
 async function StartUserTest(userId, questionIds, timeStamp) {
     // let connection = GetDbConnection()
-    userId = '65c627fa2068c3cadd3f5a06'
     try {
         let existingTest = await UserTests.find({user_id: userId, is_completed: 0})
         if (existingTest == null) {
             throw new Error("You have an existing on going test")
         }
         let questionIdsObj = []
-        console.log(questionIds)
         for (let i = 0; i < questionIds.length; i++) {
             questionIdsObj.push(new mongoose.Types.ObjectId(questionIds[i]))
         }
-        console.log(questionIdsObj)
         const newUserTest = new UserTests({
             user_id: new mongoose.Types.ObjectId(userId),
             question_bank: questionIdsObj,
@@ -56,7 +54,6 @@ async function StartUserTest(userId, questionIds, timeStamp) {
 
         });
         let newTest = await newUserTest.save();
-        console.log(newTest._id.toString())
         return newTest._id.toString();
     } catch (error) {
         console.error('Error:', error);
@@ -69,12 +66,11 @@ async function StartUserTest(userId, questionIds, timeStamp) {
 async function EndUserTest(userId, userTestId, timeStamp, questionIdAnswersMap) {
     // let connection = GetDbConnection()
     try {
-        userId = '65c627fa2068c3cadd3f5a06'
         let existingTest = await UserTests.findOne({_id: userTestId, user_id: userId, is_completed: 0})
         if (existingTest == null) {
             throw new Error("You have no existing test going on")
         }
-        if ((timeStamp - existingTest.start_time) / 60  > 10) {
+        if ((timeStamp - existingTest.start_time) / 60000  > 10) {
             existingTest.end_time = timeStamp
             existingTest.is_completed = true
             existingTest.correct_questions = []
@@ -82,25 +78,25 @@ async function EndUserTest(userId, userTestId, timeStamp, questionIdAnswersMap) 
             existingTest.save()
             return 0
         }
-        let questionIdsStrList = questionIdAnswersMap.keys
+        let questionIdsStrList = Object.keys(questionIdAnswersMap)
         // set diff of existingTest question_bank and questionIdsStrList must be 0
         let questionsIds = []
-
         for (let i = 0; i < questionIdsStrList.length; i++) {
-            questionsIds.push(Types.ObjectId(questionIdsStrList[i]))
+            questionsIds.push(new mongoose.Types.ObjectId(questionIdsStrList[i]))
         }
 
         let questions = await Question.find({_id: { $in: questionsIds }})
-
-        if (questions == null && questions.length === 10) {
+        if (questions == null && questions.length !== 10) {
 
         }
         correctQuestionsIds = []
         wrongQuestionsIds = []
 
+
         for(let i=0; i<questions.length; i++) {
-            questionIdStr = questions[i].toString()
-            if (questionIdAnswersMap[questionIdStr] != null){
+            questionIdStr = questions[i]._id.toString()
+            console.log(questionIdStr)
+            if (questionIdAnswersMap[questionIdStr] !== null){
                 if (questionIdAnswersMap[questionIdStr] === questions[i].answer_key) {
                     correctQuestionsIds.push(questions[i]._id)
                 } else {
@@ -111,13 +107,15 @@ async function EndUserTest(userId, userTestId, timeStamp, questionIdAnswersMap) 
             }
         }
 
+
         existingTest.end_time = timeStamp
         existingTest.score = correctQuestionsIds.length
         existingTest.is_completed = true
         existingTest.correct_questions = correctQuestionsIds
         existingTest.wrong_questions = wrongQuestionsIds
-        existingTest.save()
-        return existingTest.score
+        await existingTest.save({})
+        console.log(correctQuestionsIds.length)
+        return correctQuestionsIds.length
 
     } catch (error) {
         console.error('Error:', error);
@@ -127,8 +125,9 @@ async function EndUserTest(userId, userTestId, timeStamp, questionIdAnswersMap) 
     }
 }
 
-function GetPastUserTests(userId, timeStamp) {
+function GetPastUserTests(userId) {
     // two objects, one a list of user test and the other is the performance analysis
+
 }
 
 module.exports = {GetQuestions, StartUserTest, EndUserTest}
